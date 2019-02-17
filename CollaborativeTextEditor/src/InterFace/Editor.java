215ecaf6a311336c342a6dc9726700451d312f7a
arshdeepdhillon <arshdeep.dhillon1@ucalgary.ca>
2019-02-17 01:45:32 -0700

    added editor, but need to add cursor handler

--- CollaborativeTextEditor/src/InterFace/Editor.java
+++ CollaborativeTextEditor/src/InterFace/Editor.java
@@ -1,7 +1,14 @@
 package InterFace;
 
 import javafx.application.Application;
+import javafx.scene.Cursor;
+import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.event.EventHandler;
 import javafx.scene.web.HTMLEditor;
 import javafx.stage.Stage;
 
@@ -13,10 +20,38 @@ public class Editor extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		HTMLEditor htmlEditor = new HTMLEditor();
+		htmlEditor.setPrefHeight(360);
+		htmlEditor.setPrefWidth(360);
+
+		htmlEditor.addEventHandler(KeyEvent.ANY, new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode().equals(KeyCode.UNDEFINED)) {
+					System.out.println("Key Consumed: because its a UNDEFINED Key");
+					event.consume();
+				} else {
+					new EditorController(event);
+				}
+			}
+		});
+
 		Scene scene = new Scene(htmlEditor);
+
+		htmlEditor.addEventHandler(MouseEvent.ANY, new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				new EditorController(event);
+			}
+		});
+		
+		// add cursor handler
+		
+		
+		stage.setTitle("TypeIt");
+		stage.getIcons().add(new Image("InterFace/transparent-icon.png"));
 		stage.setScene(scene);
 		stage.show();
-		
+
 	}
 
 }
--- CollaborativeTextEditor/src/InterFace/EditorController.java
+++ CollaborativeTextEditor/src/InterFace/EditorController.java
@@ -1,8 +1,44 @@
 package InterFace;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 
 public class EditorController {
+	private MouseEvent mouseEvent;
+	private KeyEvent keyEvent;
 
+	public EditorController(KeyEvent kEvent) {
+		this.keyEvent = kEvent;
+		doKeyEvent();
+	}
+
+	public EditorController(MouseEvent mEvent) {
+		this.mouseEvent = mEvent;
+		doMouseEvent();
+	}
+
+	private void doKeyEvent() {
+		String type = keyEvent.getEventType().getName();
+		KeyCode kCode = keyEvent.getCode();
+
+		System.out.println("\ttyped: [" + type + "] Code: [" + kCode + "]");
+	}
+
+	private void doMouseEvent() {
+		String source = mouseEvent.getSource().getClass().getSimpleName();
+		String target = mouseEvent.getTarget().getClass().getSimpleName();
+
+		// get mouse x and y position relative to scene
+		double sourceSceX = mouseEvent.getSceneX();
+		double targetSceY = mouseEvent.getSceneY();
+
+		// get mouse x and y position relative to screen
+		// double sourceScrX = mouseEvent.getScreenX();
+		// double targetScrY = mouseEvent.getScreenY();
+
+		// System.out.println("type: [" + mouseEvent.getButton().name() + "] source: [" + source + "] target: [" + target + "]  sourceSceX: [" + sourceSceX + "] targetSceY: [" + targetSceY + "]" + "sourceScrX: [" + sourceScrX + "] targetScrY: [" + targetScrY + "]");
+		System.out.println("type: [" + mouseEvent.getButton().name() + "] source: [" + source + "] target: [" + target + "]  sourceSceX: [" + sourceSceX + "] targetSceY: [" + targetSceY + "]");
+
+	}
 }
