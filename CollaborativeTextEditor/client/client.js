944c577ee4101163fa61af1aecbaef06e8d64216
arshdeepdhillon <arshdeep.dhillon1@ucalgary.ca>
2019-04-01 14:51:32 -0600

    added Quill (which is an editor)
    It has basic tools.

--- CollaborativeTextEditor/client/client.js
+++ CollaborativeTextEditor/client/client.js
@@ -1,43 +1,46 @@
 $(document).ready((e) => {
-    window.typeit = window.typeit || {};
+  window.typeit = window.typeit || {};
 
-    (function() {
-        let self = this;
+  (function() {
+    let self = this;
 
-        self.init = function() {
+    self.init = function() {
 
-            $('#login input[name="submit"]').click((e) => {
-                window.typeit.login($('#login input[name="username"]').val(), $('#login input[name="password"]').val(), (err) => {
-                    if (err) {
-                        alert('invalid username/password');
-                    } else {
-                        $('#login').hide();
-                        $('#doc').show();
-                    }
-                });
+      $('#login input[name="submit"]').click((e) => {
+        window.typeit.login($('#login input[name="username"]').val(), $('#login input[name="password"]').val(), (err) => {
+          if (err) {
+            alert('invalid username/password');
+          } else {
+            $('#login').hide();
+            $('#doc').show();
+            var quill = new Quill('#editor', {
+              theme: 'snow'
             });
-        };
+          }
+        });
+      });
+    };
 
-        self.login = function(username, password, callback) {
-            console.log('sending login request');
-            $.ajax({
-                url: '/login',
-                type: 'POST',
-                data: {
-                    username: username,
-                    password: password
-                },
-                success: function() {
-                    callback();
-                },
-                error: function(e) {
-                    callback(e);
-                }
-            });
-        };
-    }).apply(window.typeit);
+    self.login = function(username, password, callback) {
+      console.log('sending login request');
+      $.ajax({
+        url: '/login',
+        type: 'POST',
+        data: {
+          username: username,
+          password: password
+        },
+        success: function() {
+          callback();
+        },
+        error: function(e) {
+          callback(e);
+        }
+      });
+    };
+  }).apply(window.typeit);
 
 
-    console.log("Starting client...");
-    typeit.init();
+  console.log("Starting client...");
+  typeit.init();
 });
